316d0e7b947e4727d74240956e879a91d63ff37c
eriksmatulis <e.ralfs.matulis@gmail.com>
2025-09-26 01:27:07 +0300

    Added CTA and Team patterns

---
+++ public/wp-content/themes/my-first-block-theme/patterns/cta.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Title: Custom Call to Action
+ * Slug: my-first-block-theme/my-cta
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Hey you! Get this recipe on your dish, you won't regret it!</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">We've picked the best calorie tips for you. Come check them out!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Go to our list of goodies!</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
---
+++ public/wp-content/themes/my-first-block-theme/patterns/team.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Title: My Team
+ * Slug: myblocks/my-team
+ * Categories: columns
+ */
+?>
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Meet our team!</h2>
+<!-- /wp:heading -->
+<!-- wp:columns {"backgroundColor":"secondary2"} -->
+<div class="wp-block-columns has-secondary-2-background-color has-background"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","style":{"shadow":"var:preset|shadow|sharp"}} -->
+<figure class="wp-block-image size-large"><img src="https://www.w3schools.com/howto/img_avatar.png" alt="Team member profile image." style="box-shadow:var(--wp--preset--shadow--sharp)"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>John Schmitt<br>Chief editor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","style":{"shadow":"var:preset|shadow|sharp"}} -->
+<figure class="wp-block-image size-large"><img src="https://www.w3schools.com/howto/img_avatar.png" alt="Team member profile image." style="box-shadow:var(--wp--preset--shadow--sharp)"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Adrian Carmack<br>Tech support</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","className":"is-style-default","style":{"shadow":"var:preset|shadow|sharp"}} -->
+<figure class="wp-block-image size-large is-style-default"><img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Team profile image." style="box-shadow:var(--wp--preset--shadow--sharp)"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Sarah Myers<br>Marketing</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
