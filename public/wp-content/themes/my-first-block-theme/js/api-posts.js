4d646c914fbb7252a834c7cb5b88a11973fc3529
eriksmatulis <e.ralfs.matulis@gmail.com>
2025-10-15 23:23:45 +0300

    Added a new template rest-posts.php to query all posts using the REST API call. Also added custom CSS for rendered REST call.

--- public/wp-content/themes/my-first-block-theme/js/api-posts.js
+++ public/wp-content/themes/my-first-block-theme/js/api-posts.js
@@ -4,6 +4,17 @@ fetch(window.location.origin + '/wp-json/wp/v2/posts')
     const container = document.getElementById('api-posts');
     posts.forEach(post => {
       const div = document.createElement('div');
+      div.setAttribute("style", 
+        `background-color: white; 
+        max-width: 50%; 
+        margin: auto; 
+        color: black; 
+        border-radius: 5px;
+        padding-left: 1em;
+        padding-bottom: 0.5em;
+        text-align: center;
+        `
+      );
       div.innerHTML = `<h3>${post.title.rendered}</h3><p>${post.excerpt.rendered}</p>`;
       container.appendChild(div);
     });
---
+++ public/wp-content/themes/my-first-block-theme/patterns/rest-posts.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Title: My REST Block
+ * Slug: my-first-block-theme/my-rest
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div id="api-posts"></div>
+<script src="<?php echo get_template_directory_uri(); ?>/js/api-posts.js"></script>
+<!-- /wp:group -->
